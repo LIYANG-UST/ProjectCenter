f1dee37313e1e9defe3ce46f006578a47e568dcf
liyang <42539774+LIYANG-UST@users.noreply.github.com>
2022-10-25 17:31:25 +0800

    :sparkles: Init

---
+++ src/pages/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Button } from "@chakra-ui/react";
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+      <Button>Add new contract</Button>
+    </div>
+  );
+};
+
+export default MainPage;
